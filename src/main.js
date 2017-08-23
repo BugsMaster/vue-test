ffe8b723b095805bc33612466c6fd7118ef9ccbc
johndean <499650191@qq.com>
2017-08-23 17:53:30 +0800

    sb

--- src/main.js
+++ src/main.js
@@ -10,16 +10,15 @@ Vue.use(VueRouter);
 import routerConfig from './router.config.js'
 // 运动模块
 import './css/animate.css';
-    //生成路由实例
-    const router=new VueRouter(routerConfig);
-    Vue.filter('date',function(input){
-         var oDate=new Date(input);
-         return oDate.getFullYear()+'-'+(oDate.getMonth()+1)+'-'+oDate.getDate()+' '+oDate.getHours()+':'+oDate.getMinutes()+':'+oDate.getSeconds();
-    });
-    //最后挂到vue上
-    var vm = new Vue({
-        router,
-        render: h => h(App)
-    }).$mount('#app');
-console.log(410711196807211548)
-  
+//生成路由实例
+const router = new VueRouter(routerConfig);
+Vue.filter('date', function(input) {
+    var oDate = new Date(input);
+    return oDate.getFullYear() + '-' + (oDate.getMonth() + 1) + '-' + oDate.getDate() + ' ' + oDate.getHours() + ':' + oDate.getMinutes() + ':' + oDate.getSeconds();
+});
+//最后挂到vue上
+var vm = new Vue({
+    router,
+    render: h => h(App)
+}).$mount('#app');
+console.log(410711196807211548111111)
